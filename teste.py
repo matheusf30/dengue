c19c1d13112e44097940ed1f4c115269b3a86a91
Matheus Ferreira de Souza <151486309+matheusf30@users.noreply.github.com>
2023-12-12 12:39:42 -0300

    Create teste.py
    Script para abrir e testar os arquivos

---
+++ teste.py
@@ -0,0 +1,90 @@
+import pandas as pd
+
+### Encaminhamento ao Diretório "DADOS" e "RESULTADOS"
+caminho_dados = "/home/sifapsc/scripts/matheus/dados/"
+caminho_imagens = "/home/sifapsc/scripts/matheus/resultado_imagens/"
+caminho_correlacao = "/home/sifapsc/scripts/matheus/resultado_correlacao/"
+
+### Renomeação variáveis pelos arquivos
+## Dados "Brutos"
+"""
+casos = "casos.csv"
+focos = "focos.csv"
+merge = "merge_novo.csv"
+tmax = "tmax.csv"
+tmed = "tmed.csv"
+tmin = "tmin.csv"
+"""
+## Série Histórica / Semana Epidemiológica
+
+casos = "casos_se.csv"
+focos = "focos_se.csv"
+merge = "merge_se.csv"
+tmax = "tmax_se.csv"
+tmed = "tmed_se.csv"
+tmin = "tmin_se.csv"
+
+## (à partie de 2021 / Semana Epidemiológica
+"""
+casos = "casos21se.csv"
+focos = "focos21se.csv"
+merge = "merge21se.csv"
+tmax = "tmax21se.csv"
+tmed = "tmed21se.csv"
+tmin = "tmin21se.csv"
+"""
+### Abrindo Arquivos
+casos = pd.read_csv(f"{caminho_dados}{casos}")
+focos = pd.read_csv(f"{caminho_dados}{focos}")
+merge = pd.read_csv(f"{caminho_dados}{merge}")
+tmax = pd.read_csv(f"{caminho_dados}{tmax}")
+tmed = pd.read_csv(f"{caminho_dados}{tmed}")
+tmin = pd.read_csv(f"{caminho_dados}{tmin}")
+
+### Printando Informações
+print("\n \n FOCOS DE _Aedes aegypti_ \n")
+print(focos.info())
+print("~"*80)
+print(focos.dtypes)
+print("~"*80)
+print(focos.iloc[105:574, :])
+print("="*80)
+
+print("\n \n CASOS DE DENGUE \n")
+print(casos.info())
+print("~"*80)
+print(casos.dtypes)
+print("~"*80)
+print(casos)
+print("="*80)
+
+print("\n \n PRECIPITAÇÃO \n")
+print(merge.info())
+print("~"*80)
+print(merge.dtypes)
+print("~"*80)
+print(merge.iloc[710:, :])
+print("="*80)
+
+print("\n \n TEMPERATURA MÍNIMA \n")
+print(tmin.info())
+print("~"*80)
+print(tmin.dtypes)
+print("~"*80)
+print(tmin.iloc[732:, :])
+
+print("\n \n TEMPERATURA MÉDIA \n")
+print(tmed.info())
+print("~"*80)
+print(tmed.dtypes)
+print("~"*80)
+print(tmed.iloc[732:, :])
+print("="*80)
+
+print("\n \n TEMPERATURA MÁXIMA \n")
+print(tmax.info())
+print("~"*80)
+print(tmax.dtypes)
+print("~"*80)
+print(tmax.iloc[732:, :])
+print("="*80)
