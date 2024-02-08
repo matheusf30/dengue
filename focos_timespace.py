30e6c4d6b318db7f4845496aff37aad08de10fb5
matheusf30 <mfsouza90@gmail.com>
2024-02-08 01:25:52 -0300

    Apenas convertido datetime64[ns]

--- focos_timespace.py
+++ focos_timespace.py
@@ -1,6 +1,6 @@
 ### Bibliotecas Correlatas
 import pandas as pd
-#import geopandas as gpd
+import geopandas as gpd
 #import matplotlib.pyplot as plt
 
 ### Encaminhamento ao Diretório "DADOS" e "RESULTADOS"
@@ -17,6 +17,25 @@ focos_timespace = "focos_se_timespace.csv" # Semanas Epidemiológicas
 focos_timespace = pd.read_csv(f"{caminho_dados}{focos_timespace}")
 #focos_timespace = gpd.read_file(f"{caminho_dados}{focos_timespace}")
 
+### Convertendo Tipagens de Variáveis
+# Datetime64[ns]
+focos_timespace["Semana"] = pd.to_datetime(focos_timespace["Semana"])
+focos_timespace = focos_timespace.sort_values(by = ["Semana"])
+# Geometry
+"""
+crs = {"proj" : "latlong",
+       "ellps" : "WGS84",
+       "datum" : "WGS84",
+       "no_defs" : True}
+crs = crs,
+
+# Filter out rows with MULTIPOLYGON geometries
+focos_timespace = focos_timespace[focos_timespace['geometry'].geom_type == 'POLYGON']
+
+# Now try creating a GeoDataFrame again
+focos_timespace = gpd.GeoDataFrame(focos_timespace)
+#focos_timespace = gpd.GeoDataFrame(focos_timespace, geometry = "geometry")
+"""
 ### Exibindo Informações
 print("\n \n FOCOS DE _Aedes aegypti_ EM SANTA CATARINA - SÉRIE HISTÓRICA (DIVE/SC) \n + MUNICÍPIOS DE SANTA CATARINA (IBGE + centróide) \n")
 print(focos_timespace.info())
