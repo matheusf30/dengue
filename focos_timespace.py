1af405534c11881241161cbac25abec51a1aa111
matheusf30 <mfsouza90@gmail.com>
2024-02-09 12:59:23 -0300

    dividindo em dois arquivos (polígono e centróide)

--- focos_timespace.py
+++ focos_timespace.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import geopandas as gpd
 #import matplotlib.pyplot as plt
+#import folium
 
 ### Encaminhamento ao Diretório "DADOS" e "RESULTADOS"
 caminho_dados = "/home/sifapsc/scripts/matheus/dados/"
@@ -12,16 +13,29 @@ caminho_correlacao = "/home/sifapsc/scripts/matheus/resultado_correlacao/"
 #focos_timespace = "focos_timespace.csv" # Data de Coleta
 focos_timespace = "focos_se_timespace.csv" # Semanas Epidemiológicas
 #focos_timespace = "SC_Municipios_2022.shp" # Shapefile (?)
+municipios = "SC_Municipios_2022.shp"
 
 ### Abrindo Arquivo
 focos_timespace = pd.read_csv(f"{caminho_dados}{focos_timespace}")
 #focos_timespace = gpd.read_file(f"{caminho_dados}{focos_timespace}")
+municipios = gpd.read_file(f"{caminho_dados}{municipios}")
 
-### Convertendo Tipagens de Variáveis
+### Convertendo e Incluindo Tipagens de Variáveis
 # Datetime64[ns]
 focos_timespace["Semana"] = pd.to_datetime(focos_timespace["Semana"])
 focos_timespace = focos_timespace.sort_values(by = ["Semana"])
 # Geometry
+focos_timespace = focos_timespace.drop(columns = ["ponto", "geometry"])
+cidades = municipios[["NM_MUN", "geometry"]]
+cidades["Município"] = cidades["NM_MUN"].str.upper()
+focos_timespace_shp = pd.merge(focos_timespace, cidades, on = "Município", how = "left")
+focos_timespace_shp = pd.merge(focos_timespace, cidades, on = "Município", how = "left")
+
+pontos = cidades.copy()
+pontos["ponto"] = pontos["geometry"].centroid
+pontos = pontos[["NM_MUN", "Município", "ponto"]]
+focos_timespace_centroide = pd.merge(focos_timespace, pontos, on = "Município", how = "left")
+
 """
 crs = {"proj" : "latlong",
        "ellps" : "WGS84",
@@ -43,4 +57,22 @@ print("~"*80)
 print(focos_timespace.dtypes)
 print("~"*80)
 print(focos_timespace)
+print("="*80)
+
+print("\n \n FOCOS DE _Aedes aegypti_ EM SANTA CATARINA - SÉRIE HISTÓRICA (DIVE/SC) \n + MUNICÍPIOS DE SANTA CATARINA (IBGE + centróide) \n")
+print("\n SHAPEFILE \n")
+print(focos_timespace_shp.info())
+print("~"*80)
+print(focos_timespace_shp.dtypes)
+print("~"*80)
+print(focos_timespace_shp)
+print("="*80)
+
+print("\n \n FOCOS DE _Aedes aegypti_ EM SANTA CATARINA - SÉRIE HISTÓRICA (DIVE/SC) \n + MUNICÍPIOS DE SANTA CATARINA (IBGE + centróide) \n")
+print("\n CENTRÓIDE \n")
+print(focos_timespace_centroide.info())
+print("~"*80)
+print(focos_timespace_centroide.dtypes)
+print("~"*80)
+print(focos_timespace_centroide)
 print("="*80)
