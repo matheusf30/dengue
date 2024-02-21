bda08e70c11f95eacae45e924a4df177bab19573
matheusf30 <mfsouza90@gmail.com>
2024-02-21 17:09:30 -0300

    Inciando teste com Gaussian Process Regression

---
+++ gpr.py
@@ -0,0 +1,71 @@
+### Bibliotecas Correlatas
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_processe.kernels import RBF, ConstantKernel as C, RationalQuadratic as RQ
+from sklearn.gaussian_processe.kernels import WhiteKernel, ExpSineSquared as Exp, DotProdutc as Lin
+"""
+import statsmodels as sm
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.linear_model import LogisticRegression
+"""
+
+### Encaminhamento ao Diretório "DADOS" e "RESULTADOS"
+caminho_dados = "/home/sifapsc/scripts/matheus/dados/"
+caminho_imagens = "/home/sifapsc/scripts/matheus/resultado_imagens/"
+caminho_correlacao = "/home/sifapsc/scripts/matheus/resultado_correlacao/"
+
+### Variáveis
+cidade = "Florianópolis"
+# Variáveis / Semanas Epidemiológicas de 2022
+focos = "focos22se.csv"
+casos = "casos22se.csv"
+merge = "merge22se.csv"
+tmin = "tmin22se.csv"
+tmed = "tmed22se.csv"
+tmax = "tmax22se.csv"
+
+### Abrindo Arquivos
+focos = pd.read_csv(f"{caminho_dados}{focos}")
+casos = pd.read_csv(f"{caminho_dados}{casos}")
+merge = pd.read_csv(f"{caminho_dados}{merge}")
+tmin = pd.read_csv(f"{caminho_dados}{tmin}")
+tmed = pd.read_csv(f"{caminho_dados}{tmed}")
+tmax = pd.read_csv(f"{caminho_dados}{tmax}")
+
+### Selecionando Município
+focos = focos[f"{cidade}"].copy()
+casos = casos[f"{cidade}"].copy()
+merge = merge[f"{cidade}"].copy()
+tmin = tmin[f"{cidade}"].copy()
+tmed = tmed[f"{cidade}"].copy()
+tmax = tmax[f"{cidade}"].copy()
+
+### Transformação em floats de menor bits
+focos = focos.astype(np.float16)
+casos = casos.astype(np.float16)
+merge = merge.astype(np.float16)
+tmin = tmin.astype(np.float16)
+tmed = tmed.astype(np.float16)
+tmax = tmax.astype(np.float16)
+
+### Montando Tabela
+dados = pd.DataFrame()
+dados["Focos"], dados["Precipitação"], dados["Casos"] = focos, merge, casos
+dados["Temperatura Mínima"], dados["Temperatura Média"], dados["Temperatura Máxima"] = tmin, tmed, tmax
+#dados["log_focos"], dados["log_precipitacao"] = np.log(focos), np.log(merge)
+#dados["log_temperatura_minima"], dados["log_temperatura_media"] = np.log(tmin), np.log(tmed)
+#dados["log_temperatura_maxima"] = np.log(tmin)
+print(f"\n \n TABELA BASE DO MUNICÍPIO DE {cidade.upper()} PARA REGRESSÃO \n")
+print(dados)
+print("~"*80)
+print(dados.info())
+print("~"*80)
+print(dados.dtypes)
+print("~"*80)
+print(dados.describe())
+print("="*80)
