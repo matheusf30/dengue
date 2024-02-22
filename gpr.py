68929c618a0e40bff8552a5d69914afdaa58f401
matheusf30 <mfsouza90@gmail.com>
2024-02-22 01:04:49 -0300

    Kernels resolvidos. ValueError: 'x' is not 1-dimensional (line 115)

--- gpr.py
+++ gpr.py
@@ -2,9 +2,11 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C, RationalQuadratic as RQ
-from sklearn.gaussian_process.kernels import WhiteKernel, ExpSineSquared as Exp, DotProduct as Lin
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel, RationalQuadratic
+from sklearn.gaussian_process.kernels import WhiteKernel, ExpSineSquared, DotProduct, Matern
 """
 import statsmodels as sm
 import seaborn as sns
@@ -79,27 +81,93 @@ print(f"\n \nARRAY BASE DO MUNICÍPIO DE {cidade.upper()} (VARIÁVEL DEPENDENTE 
 print(x)
 print("="*80)
 
-np.random.seed(0)
 
-### Instanciando o Modelo de Processo Gaussiano
+### Pré-processamento
+np.random.seed(0)
+raw_treino_x, raw_teste_x, treino_y, teste_y = train_test_split(x, y, test_size = 0.3)#, stratify = y)
+scaler = StandardScaler()
+scaler.fit(raw_treino_x)
+treino_x = scaler.transform(raw_treino_x)
+teste_x = scaler.transform(raw_teste_x)
+
+### Modelando a Regressão por Processo Gaussiano
+# Selecionando Kernel
+
+kernels = [1.0 * RBF(length_scale=1.0, length_scale_bounds=(1e-1, 10.0)),
+           1.0 * RationalQuadratic(length_scale=1.0, alpha=0.1),
+           1.0 * ExpSineSquared(length_scale=1.0, periodicity=3.0,
+                                length_scale_bounds=(0.1, 10.0),
+                                periodicity_bounds=(1.0, 10.0)),
+           ConstantKernel(0.1, (0.01, 10.0))
+               * (DotProduct(sigma_0=1.0, sigma_0_bounds=(0.1, 10.0)) ** 2),
+           1.0 * Matern(length_scale=1.0, length_scale_bounds=(1e-1, 10.0),
+                        nu=1.5)]
+# Instanciando Modelo
+for kernel in kernels:
+
+    gp = GaussianProcessRegressor(kernel=kernel)
+
+    # Plot prior
+    plt.figure(figsize=(8, 8))
+    plt.subplot(2, 1, 1)
+    y_mean, y_std = gp.predict(x, return_std=True)
+    plt.plot(x, y_mean, 'k', lw=3, zorder=9)
+    plt.fill_between(x, y_mean - y_std, y_mean + y_std,
+                     alpha=0.2, color='k')
+    y_samples = gp.sample_y(x[:, np.newaxis], 10)
+    plt.plot(x, y_samples, lw=1)
+    plt.xlim(0, 5)
+    plt.ylim(-3, 3)
+    plt.title(f"Prior (kernel: {kernel})", fontsize=12)
+
+    # Fit GP
+    gp.fit(x, y)
+    # Plot posterior
+    plt.subplot(2, 1, 2)
+    y_mean, y_std = gp.predict(x, return_std=True)
+    plt.plot(x, y_mean, 'k', lw=3, zorder=9)
+    plt.fill_between(x, y_mean - y_std, y_mean + y_std, alpha=0.2, color='k')
+
+    y_samples = gp.sample_y(X_[:, np.newaxis], 10)
+    plt.plot(x, y_samples, lw=1)
+    plt.scatter(x[:, 0], y, c='r', s=50, zorder=10, edgecolors=(0, 0, 0))
+    plt.xlim(0, 5)
+    plt.ylim(-3, 3)
+    plt.title(f"Posterior (kernel: {gp.kernel_})\n Log-Likelihood: {gp.log_marginal_likelihood(gp.kernel_.theta)}",
+              fontsize=12)
+    plt.tight_layout()
+
+plt.show()
+
 """
-kernel = C()*Exp(length_scale = 52, periodicity = 1)
-kernel = C()*RQ(length_scale = 52, alpha = 1)
-kernel = C()*Exp(length_scale = 52, periodicity = 1) * RQ(length_scale = 52, alpha = 1,
+#raise ValueError(f"{name!r} is not 1-dimensional")
+#ValueError: 'y1' is not 1-dimensional
+
+gp = GaussianProcessRegressor(kernel = kernel, n_restarts_optimizer = 4)
+gp.fit(x, y)
+y_pred_1, sigma_1 = gp.predict(x, return_std = True)
+
+kernel = C()*Exp(length_scale = 1, periodicity = 1)
+
+kernel = C()*RQ(length_scale = 1, alpha = 1)
+
+kernel = C()*Exp(length_scale = 1, periodicity = 1) * RQ(length_scale = 1, alpha = 1,
                                                           length_scale_bounds = (1e-05, 2),
                                                           alpha_bounds = (1e-05, 100000.0))
 
-kernel = C()*RBF(length_scale = 52,
+kernel = C()*RBF(length_scale = 1,
                  length_scale_bounds = (1e-05, 2)) * RQ(length_scale = 1, alpha = 0.5,
                                                         length_scale_bounds = (1e-05, 2),
-                                                        alpha_bounds = (1e-05, 100000.0) + Exp (length_scale = 52, periodicity = 1))
+                                                        alpha_bounds = (1e-05, 100000.0) + Exp (length_scale = 1, periodicity = 1))
+# GP is a distribution, not a single number.
+# Operate over a wide range of domains, and  enable efficient of hyperparameter selection.
 # Kernel = A function which measures the similarity of two inputs, x and x', written as: k(x, x'|t); (t = thao)
 # where t (thao) is a vector of hyperparameters used to tune it, how the kernel measures similarity.
 # Can't just use any function. There are technical restrictions on what makes for a valid kernel.
 # Most software won't allow you to use an invalid kernel.
 # Bold face, Lower case! x and x' are vectors!
 # 1 dimensional Radial Basis Function (RBF). Similarity with Heatmap. k(x,x'|t) = s²exp(-1/2(x-x'/l)²); (s = output_scale)(l = lenght_scale)
-# Small values of the lenght scales can makes the samles wiggle more rapidly.
+# Small values of the lenght scales can makes the samples wiggle more rapidly.
 # Output scale determines the scale of the y values. Incrising makes the function spin more of the y-axis.
 # Intuitively, a GP will sample functions with nearby y's for x's deemed similar by the kernel.
 # Modeling by Combining Kernels:
@@ -125,10 +193,4 @@ kernel = C()*RBF(length_scale = 52,
 # It's just the log density of the y vector according to some multivariate normal.
 # GPyTorch
 # The art of GPS by using neural networks to learn the kernel automatically.
-gp = GaussianProcessRegressor(kernel = kernel, n_restarts_optimizer = 4)
-gp.fit(x, y)
-y_pred_1, sigma_1 = gp.predict(x, return_std = True)
 """
-#A previsão no Processo Gaussiano é uma distribuição, não um valor.
-# Opera acima de uma margem de domínios é é eficiente com seleção de Hiperparâmetros.
-
