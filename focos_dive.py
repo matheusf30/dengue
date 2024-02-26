7d1a05e7093adc980832c897943de846952ef45e
matheusf30 <mfsouza90@gmail.com>
2024-02-26 16:11:24 -0300

    Tentando visualização no MatPlotLib

--- focos_dive.py
+++ focos_dive.py
@@ -21,6 +21,7 @@ As colunas não estão ordenadas alfabeticamente.)
 ### Bibliotecas Correlatas
 import pandas as pd
 import matplotlib.pyplot as plt
+import datetime
 import seaborn as sns
 
 ### Encaminhamento ao Diretório "DADOS" e "RESULTADOS"
@@ -30,158 +31,48 @@ caminho_correlacao = "/home/sifapsc/scripts/matheus/resultado_correlacao/"
 
 ### Renomeação variáveis pelos arquivos
 focos = "focos_pivot.csv"
+cidade = "Florianópolis"
 
 ### Abrindo Arquivo
 focos = pd.read_csv(f"{caminho_dados}{focos}")
 
-### Limpeza e Tratamento de Dados 
-focos = focosdive[["Regional", "Município", "Imóvel", "Depósito", "Data da Coleta"]]
-# HERVAL D`OESTE, PRESIDENTE CASTELO BRANCO, SÃO CRISTOVÃO DO SUL, GRÃO PARÁ, LAURO MULLER...
-trocanome = {
-"HERVAL D`OESTE": "HERVAL D'OESTE",
-"PRESIDENTE CASTELO BRANCO" : "PRESIDENTE CASTELLO BRANCO",
-"SÃO CRISTOVÃO DO SUL" : "SÃO CRISTÓVÃO DO SUL",
-"GRÃO PARÁ" : "GRÃO-PARÁ",
-"LAURO MULLER" : "LAURO MÜLLER"
-}
-focosdive["Município"] = focosdive["Município"].replace(trocanome)
-focosdive["Focos"] = 1
-focosdive["Data"] = focosdive["Data da Coleta"]
-focosdive["Data"] = pd.to_datetime(focosdive["Data"])
-focossemana = focosdive.copy()
-focossemana["Semana"] = focossemana["Data"].dt.to_period("W-SAT").dt.to_timestamp()
-focossemana = focossemana.groupby(["Semana", "Município"]).sum(numeric_only = True)["Focos"]
-focossemana = focossemana.reset_index()
-focossemana.to_csv(f"{caminho_dados}focos_se_dive.csv", index = False)
+### Pré-Processamento
+cidade = cidade.upper()
+cidades = focos.columns
 
-### Unindo Dataframe e Geodataframe
-cidades = municipios[["NM_MUN", "geometry"]]
-cidades["Município"] = cidades["NM_MUN"].str.upper()
-"""
-pontos = cidades.copy()
-crs = {"proj" : "latlong",
-       "ellps" : "WGS84",
-       "datum" : "WGS84",
-       "no_defs" : True}
-pontos = gpd.GeoDataFrame(pontos, crs = crs, geometry = )
-"""
-pontos = cidades.copy()
-pontos["ponto"] = cidades["geometry"].centroid
-pontos = pontos[["NM_MUN", "Município", "ponto"]]
-focos_timespace_poligono = pd.merge(focosdive, cidades, on = "Município", how = "left") # Data de Coleta e Polígonos
-focos_timespace_centroide = pd.merge(focosdive, pontos, on = "Município", how = "left") # Data da Coleta e Centróides
-focos_timespace_se_poligono = pd.merge(focossemana, cidades, on = "Município", how = "left") # Semanas Epidemiológicas e Polígonos
-focos_timespace_se_centroide = pd.merge(focossemana, pontos, on = "Município", how = "left")# Semanas Epidemiológicas e Centróides
-focos_timespace_poligono = focos_timespace_poligono.drop(columns = ["NM_MUN"])
-focos_timespace_centroide = focos_timespace_centroide.drop(columns = ["NM_MUN"])
-focos_timespace_se_poligono = focos_timespace_se_poligono.drop(columns = ["NM_MUN"])
-focos_timespace_se_centroide = focos_timespace_se_centroide.drop(columns = ["NM_MUN"])
-xy = pontos.copy()
-xy["latitude"] = cidades["geometry"].centroid.y
-xy["longitude"] = cidades["geometry"].centroid.x
-focos_timespace_xy = pd.merge(focossemana, xy, on = "Município", how = "left")
-focos_timespace_xy = focos_timespace_xy.drop(columns = ["NM_MUN", "ponto"])
-"""
-### Salvando Arquivos
-focos_timespace_poligono.to_csv(f"{caminho_dados}focos_timespace_poligono.csv", index = False) # Série Histórica e Polígonos
-focos_timespace_centroide.to_csv(f"{caminho_dados}focos_timespace_centroide.csv", index = False) # Série Histórica e Centróides
-focos_timespace_se_poligono.to_csv(f"{caminho_dados}focos_timespace_se_poligono.csv", index = False) # Semanas Epidemiológicas e Polígonos
-focos_timespace_se_centroide.to_csv(f"{caminho_dados}focos_timespace_se_centroide.csv", index = False) # Semanas Epidemiológicas e Centróides
-"""
-focos_timespace_xy.to_csv(f"{caminho_dados}focos_timespace_xy.csv", index = False) # Semanas Epidemiológicas, Latitudes e Longitudes
+### Visualização
+fig = plt.figure(figsize = (15, 8))
+eixo = fig.add_axes([0, 0, 1, 1])
+eixo2 = fig.add_axes([0.5, 0.65, 0.45, 0.3])
+
+eixo.plot(focos["Semana"], focos[cidade], color = "r")
+eixo.set_xlim(datetime.datetime(2020,1,1), datetime.datetime(2022,12,4))
+eixo.set_ylim(0, 600)
+eixo.set_title("Série Temporal de Temperatura no Verão de 2014 (Hemisfério Norte)", fontsize = 20, pad = 20)
+eixo.set_ylabel(f"Temperatura (K)", fontsize = 16)
+eixo.set_xlabel("Tempo (meses)", fontsize = 16)
+eixo.legend([cidade], loc = "upper left", fontsize = 14)
+eixo.grid(True)
+
+azul_esquerda = focos["Semana"] < datetime.datetime(2020,1,1)
+azul_direita = focos["Semana"] > datetime.datetime(2022,12,4)
+
+eixo2.plot(focos["Semana"], focos[cidade], color = "r")
+eixo2.plot(focos[azul_esquerda]["Semana"], focos[azul_esquerda][cidade], color = "b")
+eixo2.plot(focos[azul_direita]["Semana"], focos[azul_direita][cidade], color = "b")
+eixo2.set_xlim(datetime.datetime(2012,1,1), datetime.datetime(2022,12,25))
+eixo2.set_title("Série Temporal de Temperatura em 2014", fontsize = 15)
+eixo2.set_ylabel("Temperatura (K)", fontsize = 10)
+eixo2.set_xlabel("Tempo (meses)", fontsize = 10)
+eixo2.legend([cidade], loc = "best", fontsize = 8)
+eixo2.grid(True)
 
 ### Exibindo Informações
-print("\n \n FOCOS DE _Aedes aegypti_ EM SANTA CATARINA - SÉRIE HISTÓRICA (DIVE/SC) \n")
-print(focosdive.info())
+print("\n \n FOCOS DE _Aedes_ spp. EM SANTA CATARINA - SÉRIE HISTÓRICA (DIVE/SC) \n")
+print(focos.info())
 print("~"*80)
-print(focosdive.dtypes)
+print(focos.dtypes)
 print("~"*80)
-print(focosdive)
+print(focos)
 print("="*80)
-
-print("\n \n MUNICÍPIOS DE SANTA CATARINA (IBGE) \n")
-print(municipios.info())
-print("~"*80)
-print(municipios.dtypes)
-print("~"*80)
-print(municipios)
-print("="*80)
-
-print("\n \n CIDADES DE SANTA CATARINA (IBGE) \n")
-print(cidades.info())
-print("~"*80)
-print(cidades.dtypes)
-print("~"*80)
 print(cidades)
-print("="*80)
-
-
-print("\n \n MUNICÍPIOS DE SANTA CATARINA (IBGE- centróide) \n")
-print(pontos.info())
-print("~"*80)
-print(pontos.dtypes)
-print("~"*80)
-print(pontos)
-print("="*80)
-
-print("\n \n FOCOS DE _Aedes aegypti_ EM SANTA CATARINA - SÉRIE HISTÓRICA (DIVE/SC) SEMANAS EPIDEMIOLÓGICAS \n")
-print(focossemana.info())
-print("~"*80)
-print(focossemana.dtypes)
-print("~"*80)
-print(focossemana)
-print("="*80)
-
-print("\n \n FOCOS DE _Aedes aegypti_ EM SANTA CATARINA - SÉRIE HISTÓRICA (DIVE/SC) \n + MUNICÍPIOS DE SANTA CATARINA (IBGE - POLÍGONOS E MULTIPOLÍGONOS) \n")
-print(focos_timespace_poligono.info())
-print("~"*80)
-print(focos_timespace_poligono.dtypes)
-print("~"*80)
-print(focos_timespace_poligono)
-print("="*80)
-
-print("\n \n FOCOS DE _Aedes aegypti_ EM SANTA CATARINA - SÉRIE HISTÓRICA (DIVE/SC) \n + MUNICÍPIOS DE SANTA CATARINA (IBGE - CENTRÓIDES) \n")
-print(focos_timespace_centroide.info())
-print("~"*80)
-print(focos_timespace_centroide.dtypes)
-print("~"*80)
-print(focos_timespace_centroide)
-print("="*80)
-
-print("\n \n FOCOS DE _Aedes aegypti_ EM SANTA CATARINA - SÉRIE HISTÓRICA - SEMANAS EPIDEMIOLÓGICAS (DIVE/SC) \n + MUNICÍPIOS DE SANTA CATARINA (IBGE - POLÍGONOS E MULTIPOLÍGONOS) \n")
-print(focos_timespace_se_poligono.info())
-print("~"*80)
-print(focos_timespace_se_poligono.dtypes)
-print("~"*80)
-print(focos_timespace_se_poligono)
-print("="*80)
-
-print("\n \n FOCOS DE _Aedes aegypti_ EM SANTA CATARINA - SÉRIE HISTÓRICA - SEMANAS EPIDEMIOLÓGICAS (DIVE/SC) \n + MUNICÍPIOS DE SANTA CATARINA (IBGE - CENTRÓIDES) \n")
-print(focos_timespace_se_centroide.info())
-print("~"*80)
-print(focos_timespace_se_centroide.dtypes)
-print("~"*80)
-print(focos_timespace_se_centroide)
-print("="*80)
-
-print("\n \n FOCOS DE _Aedes aegypti_ EM SANTA CATARINA - SÉRIE HISTÓRICA - SEMANAS EPIDEMIOLÓGICAS (DIVE/SC) \n + MUNICÍPIOS DE SANTA CATARINA (Lat/Lon) \n")
-print("\n XY - Latitude e Longitude \n")
-print(focos_timespace_xy.info())
-print("~"*80)
-print(focos_timespace_xy.dtypes)
-print("~"*80)
-print(focos_timespace_xy)
-print("="*80)
-
-"""
-rows_with_nan = focos_timespace[focos_timespace.isna().any(axis=1)]
-# Display rows with NaN values
-print(rows_with_nan)
-# HERVAL D`OESTE, PRESIDENTE CASTELO BRANCO, SÃO CRISTOVÃO DO SUL, GRÃO PARÁ, LAURO MULLER... 
-
-#focos_timespace.to_csv(f"{caminho_dados}focos_timespace.csv", index = False) # Data de Coleta
-#focos_timespace.to_csv(f"{caminho_dados}focos_se_timespace.csv", index = False) # Semanas Epidemiológicas
-#focos_timespace.to_file(f"{caminho_dados}focos_timespace.shp") 
-"""
-municipios.plot()
-plt.show()
