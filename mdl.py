f99085afb8350da338728d7603f2c04f7f831450
matheusf30 <mfsouza90@gmail.com>
2024-02-28 15:52:40 -0300

    ValueError: Exception encountered when calling layer 'sequential' (type Sequential).

--- mdl.py
+++ mdl.py
@@ -1,8 +1,13 @@
 ### Bibliotecas Correlatas
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-import datetime
-import seaborn as sns
+#import datetime
+#import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import tensorflow
+from tensorflow import keras
 
 ### Encaminhamento ao Diretório "DADOS" e "RESULTADOS"
 caminho_dados = "/home/sifapsc/scripts/matheus/dados_dengue/"
@@ -31,9 +36,78 @@ lista_cidades = ["FLORIANÓPOLIS", "CHAPECÓ", "JOINVILLE", "ITAJAÍ"]
 cidade = cidade.upper()
 cidades = focos.columns
 focos['Semana'] = pd.to_datetime(focos['Semana'])#, format="%Y%m%d")
+casos['Semana'] = pd.to_datetime(casos['Semana'])
+prec['Semana'] = pd.to_datetime(prec['Semana'])
+tmin['Semana'] = pd.to_datetime(tmin['Semana'])
+tmed['Semana'] = pd.to_datetime(tmed['Semana'])
+tmax['Semana'] = pd.to_datetime(tmax['Semana'])
+"""
+prec_cidade_2012 = prec[cidade].iloc[605: ]
+"""
 
+### Montando Dataset
+dataset = tmin[["Semana"]].copy()
+dataset["TMIN"] = tmin[cidade].copy()
+dataset["TMED"] = tmed[cidade].copy()
+dataset["TMAX"] = tmax[cidade].copy()
+dataset = dataset.merge(prec[["Semana", cidade]], how = "left", on = "Semana").copy()
+dataset = dataset.merge(focos[["Semana", cidade]], how = "left", on = "Semana").copy()
+troca_nome = {f"{cidade}_x" : "PREC", f"{cidade}_y" : "FOCOS"}
+dataset = dataset.rename(columns = troca_nome)
+dataset.dropna(inplace = True)
+dataset.set_index("Semana", inplace = True)
+dataset.columns.name = f"{cidade}"
+
+### Dividindo Dataset em Treino e Teste
+SEED = np.random.seed(0)
+x = dataset.drop(columns = "FOCOS")
+y = dataset["FOCOS"]
+
+treino_x, teste_x, treino_y, teste_y = train_test_split(x, y,
+                                                        random_state = SEED,
+                                                        test_size = 0.25)#,
+                                                        #stratify = y)
+
+### Normalizando Dataset_x
+scaler = StandardScaler()
+scaler.fit(treino_x)
+treino_normal_x = scaler.transform(treino_x)
+teste_normal_x = scaler.transform(teste_x)
+
+### Instanciando e Compilando Modelo
+input_flatten = keras.Input(shape = treino_normal_x.shape)
+shape_input = keras.layers.Flatten()(input_flatten)
+#shape_input = keras.layers.Reshape(treino_normal_x.shape)(input_flatten)
+
+modelo = keras.Sequential([
+    #keras.layers.GlobalMaxPooling1D(input_shape = treino_normal_x.shape),
+    keras.layers.UpSampling2D(size = treino_normal_x.shape, data_format = None, interpolation = "nearest"),
+    #keras.layers.Flatten(input_shape = treino_normal_x.shape), #entrada. #Camada 0
+    keras.layers.Dense(256, activation = tensorflow.nn.relu), #processamento. #Camada 1
+    keras.layers.Dropout(0.2), # ~Normalização #Camada2
+    keras.layers.Dense(len(y), activation = tensorflow.nn.softmax)]) #saida. #Camada 3
+
+modelo.compile(optimizer = "adam",
+               loss = "sparse_categorical_crossentropy",
+               metrics = ["accuracy"])
+
+### Testando e Validando Modelo
+valida = modelo.fit(treino_normal_x, treino_y, epochs = 5, validation_split = 0.2)
+
+### Visualização Gráfica
+plt.plot(valida.history["accuracy"])
+plt.plot(valida.history["val_accuracy"])
+plt.title("Acurácia por Épocas (Ciclos de Treino)")
+plt.plot(valida.history["loss"])
+plt.plot(valida.history["val_loss"])
+plt.title("Perda por Épocas (Ciclos de Treino)")
+plt.xlabel("Ciclos")
+plt.ylabel("Perda e Acurácia")
+plt.legend(["Acurácia_Treino", "Acurácia_Validação", "Perda_Treino", "Perda_Validação"])
+plt.show()
 
 ### Exibindo Informações
+"""
 print("\n \n CASOS DE DENGUE EM SANTA CATARINA - SÉRIE HISTÓRICA (DIVE/SC) \n")
 print(casos.info())
 print("~"*80)
@@ -81,3 +155,25 @@ print(tmax.dtypes)
 print("~"*80)
 print(tmax)
 print("="*80)
+"""
+print("\n \n CONJUNTO DE DADOS PARA TREINO E TESTE \n")
+print(dataset.info())
+print("~"*80)
+print(dataset.dtypes)
+print("~"*80)
+print(dataset)
+print("="*80)
+print(f"X: {x}.")
+print("="*80)
+print(f"Y: {y}.")
+print("="*80)
+print(f"Treinaremos com {len(treino_x)} elementos e testaremos com {len(teste_x)} elementos.") # Tamanho é igual para dados normalizados
+print(f"Formato dos dados (X) nas divisões treino: {treino_x.shape} e teste: {teste_x.shape}.")
+print(f"Formato dos dados (Y) nas divisões treino: {treino_y.shape} e teste: {teste_y.shape}.")
+print("="*80)
+print(f"keras.layers.Flatten(input_shape = x); onde x: {shape_input.shape}.")
+"""
+print(prec_cidade_2012)
+print(prec.iloc[605: , :])
+print(tmin.iloc[626: , :])
+"""
