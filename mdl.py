d3e8ed8b6b106842800ded61fe3e579863c70cac
matheusf30 <mfsouza90@gmail.com>
2024-03-07 14:21:55 -0300

    Automatizando Retroação.

--- mdl.py
+++ mdl.py
@@ -33,16 +33,17 @@ tmed = pd.read_csv(f"{caminho_dados}{tmed}", low_memory = False)
 tmax = pd.read_csv(f"{caminho_dados}{tmax}", low_memory = False)
 
 ### Pré-Processamento
+retroagir = 8 # Semanas Epidemiológicas
 cidade = "Florianópolis"
-lista_cidades = ["FLORIANÓPOLIS", "CHAPECÓ", "JOINVILLE", "ITAJAÍ"]
+#lista_cidades = ["FLORIANÓPOLIS", "CHAPECÓ", "JOINVILLE", "ITAJAÍ"]
 cidade = cidade.upper()
-cidades = focos.columns
-focos['Semana'] = pd.to_datetime(focos['Semana'])#, format="%Y%m%d")
-casos['Semana'] = pd.to_datetime(casos['Semana'])
-prec['Semana'] = pd.to_datetime(prec['Semana'])
-tmin['Semana'] = pd.to_datetime(tmin['Semana'])
-tmed['Semana'] = pd.to_datetime(tmed['Semana'])
-tmax['Semana'] = pd.to_datetime(tmax['Semana'])
+cidades = focos.columns#.drop(columns = "Semana", inplace = True)
+focos["Semana"] = pd.to_datetime(focos["Semana"])#, format="%Y%m%d")
+casos["Semana"] = pd.to_datetime(casos["Semana"])
+prec["Semana"] = pd.to_datetime(prec["Semana"])
+tmin["Semana"] = pd.to_datetime(tmin["Semana"])
+tmed["Semana"] = pd.to_datetime(tmed["Semana"])
+tmax["Semana"] = pd.to_datetime(tmax["Semana"])
 """
 prec_cidade_2012 = prec[cidade].iloc[605: ]
 """
@@ -56,28 +57,13 @@ dataset = dataset.merge(prec[["Semana", cidade]], how = "left", on = "Semana").c
 dataset = dataset.merge(focos[["Semana", cidade]], how = "left", on = "Semana").copy()
 troca_nome = {f"{cidade}_x" : "PREC", f"{cidade}_y" : "FOCOS"}
 dataset = dataset.rename(columns = troca_nome)
-dataset["TMIN_m1"] = dataset["TMIN"].shift(-1)
-dataset["TMED_m1"] = dataset["TMED"].shift(-1)
-dataset["TMAX_m1"] = dataset["TMAX"].shift(-1)
-dataset["PREC_m1"] = dataset["PREC"].shift(-1)
-dataset["FOCOS_m1"] = dataset["FOCOS"].shift(-1)
-dataset["TMIN_m2"] = dataset["TMIN"].shift(-2)
-dataset["TMED_m2"] = dataset["TMED"].shift(-2)
-dataset["TMAX_m2"] = dataset["TMAX"].shift(-2)
-dataset["PREC_m2"] = dataset["PREC"].shift(-2)
-dataset["FOCOS_m2"] = dataset["FOCOS"].shift(-2)
-dataset["TMIN_m3"] = dataset["TMIN"].shift(-3)
-dataset["TMED_m3"] = dataset["TMED"].shift(-3)
-dataset["TMAX_m3"] = dataset["TMAX"].shift(-3)
-dataset["PREC_m3"] = dataset["PREC"].shift(-3)
-dataset["FOCOS_m3"] = dataset["FOCOS"].shift(-3)
-dataset["TMIN_m4"] = dataset["TMIN"].shift(-4)
-dataset["TMED_m4"] = dataset["TMED"].shift(-4)
-dataset["TMAX_m4"] = dataset["TMAX"].shift(-4)
-dataset["PREC_m4"] = dataset["PREC"].shift(-4)
-dataset["FOCOS_m4"] = dataset["FOCOS"].shift(-4)
+for r in range(1, retroagir + 1):
+    dataset[f"TMIN_r{r}"] = dataset["TMIN"].shift(-r)
+    dataset[f"TMED_r{r}"] = dataset["TMED"].shift(-r)
+    dataset[f"TMAX_r{r}"] = dataset["TMAX"].shift(-r)
+    dataset[f"PREC_r{r}"] = dataset["PREC"].shift(-r)
+    dataset[f"FOCOS_r{r}"] = dataset["FOCOS"].shift(-r)
 dataset.dropna(inplace = True)
-#dataset["Semana"] = pd.to_numeric(dataset["Semana"])
 dataset.set_index("Semana", inplace = True)
 dataset.columns.name = f"{cidade}"
 
@@ -85,11 +71,11 @@ dataset.columns.name = f"{cidade}"
 SEED = np.random.seed(0)
 x = dataset.drop(columns = "FOCOS")
 y = dataset["FOCOS"]
-x = x.to_numpy()
-y = y.to_numpy()
-x = x.reshape(x.shape[0], -1)
+x_array = x.to_numpy()
+y_array = y.to_numpy()
+x_array = x_array.reshape(x_array.shape[0], -1)
 
-treino_x, teste_x, treino_y, teste_y = train_test_split(x, y,
+treino_x, teste_x, treino_y, teste_y = train_test_split(x_array, y_array,
                                                         random_state = SEED,
                                                         test_size = 0.2)#,
                                                         #stratify = y)
@@ -127,7 +113,7 @@ modelo = keras.Sequential([
     #keras.layers.Dense(128, activation = tensorflow.nn.relu), #processamento. #Camada 2
     keras.layers.Dense(64, activation = tensorflow.nn.relu), #processamento. #Camada 3
     keras.layers.Dropout(0.3), # ~Normalização (processamento) #Camada 4
-    keras.layers.Dense(len(y), activation = tensorflow.nn.softmax)]) #saida. #Camada 5
+    keras.layers.Dense(len(y_array), activation = tensorflow.nn.softmax)]) #saida. #Camada 5
 
 lr_adam = keras.optimizers.Adam(learning_rate = 0.0001)
 callbacks = [keras.callbacks.EarlyStopping(monitor = "val_loss")]#,
@@ -144,28 +130,41 @@ valida = modelo.fit(treino_normal_x, treino_y,
                     epochs = 100, validation_split = 0.2,
                     callbacks = callbacks)#, batch_size = 10000)
 testes = modelo.predict(teste_x)
+#testes_normal = modelo.predict(teste_normal_x)
 #previsoes = tensorflow.argmax(testes, axis = 1)
+previsoes = modelo.predict(x)
+print(treino_x.shape[1:])
 
-print(f"Resultado do Teste do Modelo: {np.argmax(testes[0])}")#np.argmax(testes[0])
-print(f"Número de Focos do Teste: {teste_y[0]}")
+### Exibindo Informações
+print("\n \n CONJUNTO DE DADOS PARA TREINO E TESTE \n")
+print(dataset.info())
+print("~"*80)
+print(dataset.dtypes)
+print("~"*80)
+print(dataset)
+print("="*80)
+print(f"X no formato numpy.ndarray: {x_array}.")
+print("="*80)
+print(previsoes.T.shape)
+print(type(previsoes))
+print(previsoes.T)
+print("="*80)
+print(f"Treinando com {len(treino_x)} elementos e testando com {len(teste_x)} elementos.") # Tamanho é igual para dados normalizados
+print(f"Formato dos dados (X) nas divisões treino: {treino_x.shape} e teste: {teste_x.shape}.")
+print(f"Formato dos dados (Y) nas divisões treino: {treino_y.shape} e teste: {teste_y.shape}.")
+print("="*80)
 print(modelo.summary())
-"""
-print("\n PESOS[0] E VIÉSES[1] DA CAMADA 1 (dense):\n", modelo.layers[1].get_weights())
-print("~"*80)
-print("\n PESOS[0] E VIÉSES[1] DA CAMADA 2 (dense_1):\n",modelo.layers[2].get_weights())
-print("~"*80)
-print("\n PESOS[0] E VIÉSES[1] DA CAMADA 3 (dropout):\n",modelo.layers[3].get_weights())
-print("~"*80)
-print("\n PESOS[0] E VIÉSES[1] DA CAMADA 4 (dense_2):\n",modelo.layers[4].get_weights())
-print("~"*80)
-print(modelo.get_config())
+print("="*80)
+for i in range(0,500):
+    #print(f"Resultados do Teste do Modelo: {np.argmax(testes[i])}")#np.argmax(testes[0])
+    #print(f"Números de Focos do Teste: {teste_y[i]}")
+    #print("="*80)
+    print(f"Resultados da Previsão do Modelo: {np.argmax(previsoes[i])}")
+    print(f"Números de Focos do Dataset: {dataset['FOCOS'][i]}")
+    print("="*80)
 
-testes_modelo_salvo = modelo.predict(teste_normal_x)
-"""
-print(f"Resultado do Teste do Modelo: {np.argmax(testes[0])}")
-print(f"Número do Teste: {teste_y[0]}")
-"""
 ### Visualização Gráfica
+"""
 plt.plot(valida.history["accuracy"])
 plt.plot(valida.history["val_accuracy"])
 plt.plot(valida.history["loss"])
@@ -175,22 +174,38 @@ plt.xlabel("Ciclos de Treino (epochs)")
 plt.ylabel("Perda e Acurácia")
 plt.legend(["Acurácia_Treino", "Acurácia_Validação", "Perda_Treino", "Perda_Validação"])
 plt.show()
-
-### Visualização Gráfica
+"""
+final = pd.DataFrame()
+final["Semana"] = focos["Semana"]
+final["Focos"] = focos[cidade]
+for d in range(0, int(retroagir/2)):
+    final.drop(d, axis = 0, inplace = True)
+    for p in range(-1, -(int(retroagir/2) +1)):
+        final.drop(p, axis = 0, inplace = True)
+lista_previsoes = []
+for v in range(0, len(previsoes)):
+    lista_previsoes.append(np.argmax(previsoes[v]))
+#final["Previsões"] = lista_previsoes
+print(final)
+print(lista_previsoes)
+print(range(0, len(previsoes)))
+print(range(1, int(retroagir/2) + 1))
+print(range(-1, -(int(retroagir/2) +1)))
+"""
 #df_treino_x = pd.DataFrame({'x': treino_x[:, 0]})
 #df_testes = pd.DataFrame({'y': testes[:, 0]})
 #df_teste_y = pd.DataFrame({'y': teste_y})
-sns.lineplot(x = focos["Semana"], y = pd.DataFrame(treino_x), label="Treino")
-#sns.lineplot(x = focos["Semana"], label="Ajuste")
-sns.lineplot(x = focos["Semana"], y = pd.DataFrame(testes), label = "Previsão")
-sns.lineplot(x = focos["Semana"], y = pd.DataFrame(teste_y), label = "Teste")
-plt.title("MODELO, AJUSTE E PREVISÃO")
+sns.lineplot(x = dataset.index, y = pd.DataFrame(treino_x), label="Treino")
+#sns.lineplot(x = dataset.index, label="Ajuste")
+sns.lineplot(x = dataset.index, y = pd.DataFrame(previsoes), label = "Previsão")
+sns.lineplot(x = dataset.index, y = pd.DataFrame(teste_y), label = "Teste")
+sns.lineplot(x = dataset.index, y = dataset["FOCOS"], label = "Observação")
+plt.title("MODELO, OBSERVAÇÃO E PREVISÃO")
 plt.xlabel("Tempo (?)")
 plt.ylabel("Quantidade (?)")
 plt.show()
-
-### Exibindo Informações
-
+"""
+"""
 print("\n \n CASOS DE DENGUE EM SANTA CATARINA - SÉRIE HISTÓRICA (DIVE/SC) \n")
 print(casos.info())
 print("~"*80)
@@ -238,28 +253,31 @@ print(tmax.dtypes)
 print("~"*80)
 print(tmax)
 print("="*80)
-"""
-print("\n \n CONJUNTO DE DADOS PARA TREINO E TESTE \n")
-print(dataset.info())
+
+print("\n PESOS[0] E VIÉSES[1] DA CAMADA 1 (dense):\n", modelo.layers[1].get_weights())
 print("~"*80)
-print(dataset.dtypes)
+print("\n PESOS[0] E VIÉSES[1] DA CAMADA 2 (dense_1):\n",modelo.layers[2].get_weights())
 print("~"*80)
-print(dataset)
-print("="*80)
-print(f"X: {x}.")
-print("="*80)
-print(f"Y: {y}.")
-print("="*80)
-print(treino_x.shape[1:])
-print(f"Treinaremos com {len(treino_x)} elementos e testaremos com {len(teste_x)} elementos.") # Tamanho é igual para dados normalizados
-print(f"Formato dos dados (X) nas divisões treino: {treino_x.shape} e teste: {teste_x.shape}.")
-print(f"Formato dos dados (Y) nas divisões treino: {treino_y.shape} e teste: {teste_y.shape}.")
-print("="*80)
-print(testes.T.shape)
-print(type(testes))
-print(testes.T)
-print
-"""
+print("\n PESOS[0] E VIÉSES[1] DA CAMADA 3 (dropout):\n",modelo.layers[3].get_weights())
+print("~"*80)
+print("\n PESOS[0] E VIÉSES[1] DA CAMADA 4 (dense_2):\n",modelo.layers[4].get_weights())
+print("~"*80)
+print(modelo.get_config())
+
+### Visualização Gráfica
+#df_treino_x = pd.DataFrame({'x': treino_x[:, 0]})
+#df_testes = pd.DataFrame({'y': testes[:, 0]})
+#df_teste_y = pd.DataFrame({'y': teste_y})
+sns.lineplot(x = focos["Semana"], y = pd.DataFrame(treino_x), label="Treino")
+#sns.lineplot(x = focos["Semana"], label="Ajuste")
+sns.lineplot(x = focos["Semana"], y = pd.DataFrame(testes), label = "Previsão")
+sns.lineplot(x = focos["Semana"], y = pd.DataFrame(teste_y), label = "Teste")
+plt.title("MODELO, AJUSTE E PREVISÃO")
+plt.xlabel("Tempo (?)")
+plt.ylabel("Quantidade (?)")
+plt.show()
+
+
 previsto = []
 
 for i in testes:
