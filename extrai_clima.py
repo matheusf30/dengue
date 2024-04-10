5eec4436c83398143ae6a14d4287f6d622eaa68a
matheusf30 <mfsouza90@gmail.com>
2024-04-10 16:22:21 -0300

    Funcional com variáveis diferentes e salvando em arquivos.csv ({caminho_dados}{str_var}_diario_ate_{_ANO_FINAL}.csv)

--- extrai_clima.py
+++ extrai_clima.py
@@ -21,7 +21,6 @@ diretorio_dados = os.path.sep.join(diretorios[:-6])
 print(diretorio_dados)
 """
 
-
 ### Encaminhamento aos Diretórios
 try:
     _LOCAL = "IFSC" # OPÇÕES>>> "GH" "CASA" "IFSC"
@@ -40,11 +39,18 @@ except:
 
 print(f"\nOS DADOS UTILIZADOS ESTÃO ALOCADOS NOS SEGUINTES CAMINHOS:\n\n{caminho_dados}\n\n{caminho_merge}\n\n{caminho_samet}\n\n")
 
+_ANO_FINAL = "2023" # Até quando os produtos de reanálise foram compilados
+
 ### Renomeação variáveis pelos arquivos
-merge = "MERGE_CPTEC_DAILY_SB_2000_2023.nc"
-samet_tmax = "TMAX/SAMeT_CPTEC_DAILY_TMAX_SB_2000_2022.nc"
-samet_tmed = "TMED/SAMeT_CPTEC_DAILY_TMED_SB_2000_2022.nc"
-samet_tmin = "TMIN/SAMeT_CPTEC_DAILY_TMIN_SB_2000_2022.nc"
+merge = f"MERGE_CPTEC_DAILY_SB_2000_{_ANO_FINAL}.nc"
+samet_tmax = f"TMAX/SAMeT_CPTEC_DAILY_TMAX_SB_2000_2022.nc"
+samet_tmed = f"TMED/SAMeT_CPTEC_DAILY_TMED_SB_2000_2022.nc"
+samet_tmin = f"TMIN/SAMeT_CPTEC_DAILY_TMIN_SB_2000_2022.nc"
+"""
+samet_tmax = f"TMAX/SAMeT_CPTEC_DAILY_TMAX_SB_2000_{_ANO_FINAL}.nc"
+samet_tmed = f"TMED/SAMeT_CPTEC_DAILY_TMED_SB_2000_{_ANO_FINAL}.nc"
+samet_tmin = f"TMIN/SAMeT_CPTEC_DAILY_TMIN_SB_2000_{_ANO_FINAL}.nc"
+"""
 municipios = "SC_Municipios_2022.shp"
 
 ### Abrindo Arquivos
@@ -80,7 +86,8 @@ def extrair_centroides(shapefile, netcdf4, str_var):
 		valores_centroides.append(valor)
 	valores_centroides = pd.DataFrame(data = valores_centroides)
 	valores_centroides["Municipio"] = shapefile["NM_MUN"].str.upper().copy()
-	valores_centroides.drop(columns = ["nest"], inplace = True)
+	if str_var == "prec":
+		valores_centroides.drop(columns = ["nest"], inplace = True)
 	valores_centroides = valores_centroides[["Municipio", str_var]]
 	valores_tempo = netcdf4[str_var].time.values
 	valores_variavel = netcdf4[str_var].values
@@ -89,19 +96,22 @@ def extrair_centroides(shapefile, netcdf4, str_var):
 		if isinstance(linha[str_var], xr.DataArray):
 			var_valor = [x.item() if not np.isnan(x.item()) else np.nan for x in linha[str_var]]
 			var_valores.append(var_valor)
-			print(f"\n{valores_centroides['Municipio'][i]}: Finalizado!\n{i + 1} de {len(valores_centroides['Municipio'])}.")
+			print(f"\n---{str_var}---\n{valores_centroides['Municipio'][i]}: Finalizado!\n{i + 1} de {len(valores_centroides['Municipio'])}.")
 		else:
 			var_valores.append([np.nan] * len(valores_tempo))
 			print(f"\n{valores_centroides['Municipio'][i]}: NaN... Finalizado!\n{i + 1} de {len(valores_centroides['Municipio'])}.")
 	var_valores_df = pd.DataFrame(var_valores, columns = valores_tempo)
 	valores_centroides = pd.concat([valores_centroides, var_valores_df], axis = 1)
 	valores_centroides.drop(columns = [str_var], inplace = True)
+	valores_centroides.set_index("Municipio", inplace = True)
 	valores_centroides = valores_centroides.T
 	valores_centroides["Data"] = valores_centroides.index
-	valores_centroides.reset_index(drop = True, inplace = True)
-	valores_centroides.set_index("Data", inplace = True)
+	valores_centroides.reset_index(inplace = True)
+	colunas_restantes = valores_centroides.columns.drop("Data")
+	valores_centroides = valores_centroides[["Data"] + colunas_restantes.tolist()]
 	valores_centroides.columns.name = str_var
-	valores_centroides.to_csv(f"{caminho_dados}{str_var}.csv", index = False)
+	valores_centroides.rename(columns = {"index" : str_var}, inplace = True)
+	valores_centroides.to_csv(f"{caminho_dados}{str_var}_diario_ate_{_ANO_FINAL}.csv", index = False)
 	print("="*80)
 	print(netcdf4.variables[str_var][:])
 	print(netcdf4.variables["time"][:])
@@ -118,6 +128,16 @@ def extrair_centroides(shapefile, netcdf4, str_var):
 	print("="*80)
 	return valores_centroides
 
+prec = extrair_centroides(municipios, prec, "prec")
+tmax = extrair_centroides(municipios, tmax, "tmax")
+tmed = extrair_centroides(municipios, tmed, "tmed")
+tmin = extrair_centroides(municipios, tmin, "tmin")
+
+print("!!"*80)
+print(f"\nFINALIZADA ATUALIZAÇÃO\nAtualização feita em produtos de reanálise até {_ANO_FINAL}!\n(MERGE e SAMeT - tmin, tmed, tmax)")
+print("!!"*80)
+
+"""
 municipios["centroide"] = municipios["geometry"].centroid
 municipios["centroide"] = municipios["centroide"].to_crs(epsg = 4674)
 valores_centroides = []
@@ -167,3 +187,4 @@ print(valores_centroides)
 print(valores_centroides.info())
 print(valores_centroides.dtypes)
 print("="*80)
+"""
