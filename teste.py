b0fad2dba91c07713693a3021145e011f50562bd
matheusf30 <mfsouza90@gmail.com>
2024-04-17 17:10:52 -0300

    Roteiro incluindo dados atualizados, funções e variáveis pré-selecionadas.

--- teste.py
+++ teste.py
@@ -17,6 +17,22 @@ else:
 print(f"\nOS DADOS UTILIZADOS ESTÃO ALOCADOS NOS SEGUINTES CAMINHOS:\n\n{caminho_dados}\n\n")
 
 ### Renomeação variáveis pelos arquivos
+## Série Histórica / _TEMPO / Extraídos de netCDF4 (MERGE e SAMeT)
+
+_TEMPO = "semana" # "diario"
+_ANO = "2023" # Até a última compilação de dados
+"""
+casos = "casos.csv"
+focos = "focos_se.csv"
+merge = "merge_se.csv"
+"""
+casos = "casos_dive_pivot_total.csv"
+focos = "focos_pivot.csv"
+prec = f"prec_{_TEMPO}_ate_{_ANO}.csv"
+tmax = f"tmax_{_TEMPO}_ate_{_ANO}.csv"
+tmed = f"tmed_{_TEMPO}_ate_{_ANO}.csv"
+tmin = f"tmin_{_TEMPO}_ate_{_ANO}.csv"
+
 ## Dados "Brutos"
 """
 casos = "casos.csv"
@@ -28,16 +44,16 @@ tmed = "tmed.csv"
 tmin = "tmin.csv"
 """
 ## Série Histórica / Semana Epidemiológica
-
+"""
 casos = "casos.csv"
 focos = "focos_se.csv"
 merge = "merge_se.csv"
 #prec = "prec.csv" # Dados Brutos
-prec = "https://raw.githubusercontent.com/matheusf30/dados_dengue/main/prec_diario_ate_2023.csv" # Dados Brutos
+prec = "prec_diario_ate_2023.csv" # Dados Brutos
 tmax = "tmax_se.csv"
 tmed = "tmed_se.csv"
 tmin = "tmin_se.csv"
-
+"""
 ## (à partie de 2021 / Semana Epidemiológica
 """
 casos = "casos21se.csv"
@@ -49,17 +65,54 @@ tmin = "tmin21se.csv"
 """
 ### Abrindo Arquivos
 """
+
+merge = pd.read_csv(f"{caminho_dados}{merge}")
+prec = pd.read_csv(f"{caminho_dados}{prec}", low_memory = False)
+"""
 casos = pd.read_csv(f"{caminho_dados}{casos}")
 focos = pd.read_csv(f"{caminho_dados}{focos}")
-"""
-merge = pd.read_csv(f"{caminho_dados}{merge}")
-#prec = pd.read_csv(f"{caminho_dados}{prec}", low_memory = False)
-prec = pd.read_csv(f"{prec}")
+prec = pd.read_csv(f"{caminho_dados}{prec}")
 tmax = pd.read_csv(f"{caminho_dados}{tmax}")
 tmed = pd.read_csv(f"{caminho_dados}{tmed}")
 tmin = pd.read_csv(f"{caminho_dados}{tmin}")
 
-### Printando Informações
+### Visualizando Informações
+bold = "\033[1m"
+red = "\033[91m"
+green = "\033[92m"
+yellow = "\033[33m"
+blue = "\033[34m"
+magenta = "\033[35m"
+cyan = "\033[36m"
+white = "\033[37m"
+reset = "\033[0m"
+
+def visualiza_csv(csv, str_var = "None"):
+	"""
+	Função relativa a visualização dos dados e verificação de Valores Não-números (NaN) no arquivo.csv
+	Argumento:
+	- DataFrame da Variável com arquivo.csv aberto;
+	- String da variável referente ao arquivo.csv. (pode não ser informado)
+	Retorno: Visualização de dados e Exibição de mensagens para casos de haver ou não valores NaN.
+	"""
+	print(f"\n \n {green}{bold}{str_var.upper()} (extrai_clima.py){reset} \n")
+	print(csv.info())
+	print("~"*80)
+	print(csv.dtypes)
+	print("~"*80)
+	print(csv)
+	print("~"*80)
+	print(f"{cyan}{str_var.upper()} - {red}Quantidade de {magenta}NaN{red}: {prec['FLORIANÓPOLIS'].isnull().sum()}{reset}")
+	if _TEMPO == "diario":
+		print(f"{red}Os dias com Valores {magenta}NaN{red} são:\n{prec[prec['FLORIANÓPOLIS'].isna()]['Data']}{reset}")
+	elif _TEMPO == "semana":
+		print(f"{red}As semanas com Valores {magenta}NaN{red} são:\n{prec[prec['FLORIANÓPOLIS'].isna()]['Semana']}{reset}")
+	print("="*80)
+
+visualiza_csv(prec, "prec")
+visualiza_csv(tmin, "tmin")
+visualiza_csv(tmed, "tmed")
+visualiza_csv(tmax, "tmax")
 """
 print("\n \n FOCOS DE _Aedes_ sp. \n")
 print(focos.info())
@@ -78,7 +131,7 @@ print("~"*80)
 #print(casos) #jan2014-dez2022
 print(casos.iloc[417:, :]) #jan2022-dez2022
 print("="*80)
-"""
+
 print("\n \n PRECIPITAÇÃO \n")
 print(merge.info())
 print("~"*80)
@@ -96,7 +149,10 @@ print("~"*80)
 print(prec)
 print("~"*80)
 print(f"Quantidade de NaN: {prec['FLORIANÓPOLIS'].isnull().sum()}")
-print(f"Os dias com Valores NaN são:\n{prec[prec['FLORIANÓPOLIS'].isna()]['Data']}")
+if _TEMPO == "diario":
+	print(f"Os dias com Valores NaN são:\n{prec[prec['FLORIANÓPOLIS'].isna()]['Data']}")
+elif _TEMPO == "semana":
+	print(f"As semanas com Valores NaN são:\n{prec[prec['FLORIANÓPOLIS'].isna()]['Semana']}")
 print("="*80)
 
 print("\n \n TEMPERATURA MÍNIMA \n")
@@ -104,8 +160,9 @@ print(tmin.info())
 print("~"*80)
 print(tmin.dtypes)
 print("~"*80)
+print(tmin)
 #print(tmin.iloc[732:, :]) #jan2014-dez2022
-print(tmin.iloc[1149:, :]) #jan2022-dez2022
+#print(tmin.iloc[1149:, :]) #jan2022-dez2022
 print("="*80)
 
 print("\n \n TEMPERATURA MÉDIA \n")
@@ -113,8 +170,9 @@ print(tmed.info())
 print("~"*80)
 print(tmed.dtypes)
 print("~"*80)
+print(tmed)
 #print(tmed.iloc[732:, :]) #jan2014-dez2022
-print(tmed.iloc[1149:, :]) #jan2022-dez2022
+#print(tmed.iloc[1149:, :]) #jan2022-dez2022
 print("="*80)
 
 print("\n \n TEMPERATURA MÁXIMA \n")
@@ -122,6 +180,8 @@ print(tmax.info())
 print("~"*80)
 print(tmax.dtypes)
 print("~"*80)
+print(tmax)
 #print(tmax.iloc[732:, :]) #jan2014-dez2022
-print(tmax.iloc[1149:, :]) #jan2022-dez2022
+#print(tmax.iloc[1149:, :]) #jan2022-dez2022
 print("="*80)
+"""
