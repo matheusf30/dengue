5492ba99fe85aaa6271ac0379e96cec369119cf5
matheusf30 <mfsouza90@gmail.com>
2024-05-28 11:45:41 -0300

    Tentativa de colorir partições de boxplot

--- estatistica.py
+++ estatistica.py
@@ -3,18 +3,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
 import seaborn as sns
+import numpy as np
 import pymannkendall as mk
 #from sklearn.preprocessing import NormalityTest
 from scipy.stats import shapiro, normaltest
 import statsmodels.api as sm
 
+### Condicionantes
+
+_SALVAR = False
+
+_VISUALIZAR = True
+
 ### Encaminhamento aos Diretórios
-_local = "IFSC" # OPÇÕES>>> "GH" "CASA" "IFSC"
-if _local == "GH": # _ = Variável Privada
+_LOCAL = "IFSC" # OPÇÕES>>> "GH" "CASA" "IFSC"
+if _LOCAL == "GH": # _ = Variável Privada
     caminho_dados = "https://raw.githubusercontent.com/matheusf30/dados_dengue/main/"
-elif _local == "CASA":
+elif _LOCAL == "CASA":
     caminho_dados = "C:\\Users\\Desktop\\Documents\\GitHub\\dados_dengue\\"
-elif _local == "IFSC":
+elif _LOCAL == "IFSC":
     caminho_dados = "/home/sifapsc/scripts/matheus/dados_dengue/"
     caminho_estatistica = "/home/sifapsc/scripts/matheus/dengue/resultados/estatistica/"
 else:
@@ -39,6 +46,13 @@ prec = pd.read_csv(f"{caminho_dados}{prec}", low_memory = False)
 tmin = pd.read_csv(f"{caminho_dados}{tmin}", low_memory = False)
 tmed = pd.read_csv(f"{caminho_dados}{tmed}", low_memory = False)
 tmax = pd.read_csv(f"{caminho_dados}{tmax}", low_memory = False)
+
+##### Padrão ANSI ##################################
+ansi = {"bold" : "\033[1m", "red" : "\033[91m",
+        "green" : "\033[92m", "yellow" : "\033[33m",
+        "blue" : "\033[34m", "magenta" : "\033[35m",
+        "cyan" : "\033[36m", "white" : "\033[37m", "reset" : "\033[0m"}
+#################################################################################
 
 ### Pré-Processamento
 cidade = cidade.upper()
@@ -78,10 +92,94 @@ def visualiza_focos(cidade, focos):
 	plt.show()
 
 def tendencia(cidade, csv):
+	"""
+	Função para avaliar tendência no conjunto de dados.
+	Entrada: Nome da cidade (str.upper()) e Conjunto de dados (.csv);
+	Retorno: em tela (terminal), visualização do teste para determinada cidade.
+	Obs.: Aqui a sazonalidade está incluída!
+	"""
 	mannkendall = mk.original_test(csv[cidade])
 	print(f"\nMANN KENDALL DE {cidade} = {mannkendall}.\n")
 
+def histograma(cidade, csv, str_var):
+	"""
+	"""
+	fig, axs = plt.subplots(2, 1, figsize=(12, 8), layout="constrained", frameon=False, gridspec_kw={'height_ratios': [3, 1]})
+
+	# Calculate quartiles
+	quartiles = np.percentile(csv[cidade], [25, 50, 75])
+
+	# Plot histogram
+	n, bins, patches = axs[0].hist(csv[cidade], bins='auto', color='blue', alpha=0.7, rwidth=0.85, label="Histograma")
+
+	# Assign colors to histogram bars based on quartiles
+	for patch, bin_value in zip(patches, bins):
+		if bin_value <= quartiles[0]:
+			patch.set_facecolor('green')  # Color for first quartile
+		elif bin_value <= quartiles[1]:
+			patch.set_facecolor('blue')   # Color for second quartile
+		elif bin_value <= quartiles[2]:
+			patch.set_facecolor('orange') # Color for third quartile
+		else:
+			patch.set_facecolor('red')    # Color for fourth quartile
+
+	axs[0].legend(loc="upper right")
+	axs[0].grid(True)
+
+	# Plot boxplot
+	boxplot_parts = axs[1].boxplot(csv[cidade], vert=False, patch_artist=True)
+
+    # Set colors for boxplot components based on quartiles
+	for patch in boxplot_parts['boxes']:
+		median = np.median(patch.get_xdata())  # Get the median of the box
+		if median <= quartiles[0]:
+			patch.set_facecolor('green')   # Color for first quartile
+		elif median <= quartiles[1]:
+			patch.set_facecolor('blue')    # Color for second quartile
+		elif median <= quartiles[2]:
+			patch.set_facecolor('orange')  # Color for third quartile
+		else:
+			patch.set_facecolor('red')     # Color for fourth quartile
+
+	for whisker in boxplot_parts['whiskers']:
+		median = np.median(whisker.get_xdata())  # Get the median of the whisker
+		if median <= quartiles[0]:
+			whisker.set_color('green')   # Color for first quartile
+		elif median <= quartiles[1]:
+			whisker.set_color('blue')    # Color
+
+	# Set grid for boxplot
+	axs[1].grid(True)
+
+	# Set title and labels
+	fig.suptitle(f"Distribuição: {cidade} - {str_var.upper()}")
+	plt.xlabel("Valor")
+	if _SALVAR == True:
+		_cidade = cidade.copy()
+		troca = {'Á': 'A', 'Â': 'A', 'À': 'A', 'Ã': 'A', 'Ä': 'A',
+		         'É': 'E', 'Ê': 'E', 'È': 'E', 'Ẽ': 'E', 'Ë': 'E',
+		         'Í': 'I', 'Î': 'I', 'Ì': 'I', 'Ĩ': 'I', 'Ï': 'I',
+		         'Ó': 'O', 'Ô': 'O', 'Ò': 'O', 'Õ': 'O', 'Ö': 'O',
+		         'Ú': 'U', 'Û': 'U', 'Ù': 'U', 'Ũ': 'U', 'Ü': 'U',
+		         'Ç': 'C', " " : "_", "'" : "_", "-" : "_"}
+		for velho, novo in troca.items():
+			_cidade = _cidade.replace(velho, novo)
+			plt.savefig(f"{caminho_estatistica}histograma_{str_var.upper()}_{_cidade}.pdf", format = "pdf", dpi = 1200)
+			print(f"\n\nSALVO COM SUCESSO!\n\n{caminho_estatistica}histograma_{str_var.upper()}_{cidade}.pdf\n\n")
+	if _VISUALIZAR == True:
+		print(f"\n\nVISUALIZANDO:\n\n{caminho_estatistica}histograma_{str_var.upper()}_{cidade}.pdf\n\n")
+		plt.show()
+		print(f"\n\nENCERRADO:\n\n{caminho_estatistica}histograma_{str_var.upper()}_{cidade}.pdf\n\n")
+
+
 def teste_normal(cidade, csv, str_var):
+	"""
+	Função para avaliar decomposição sazonal e tendência no conjunto de dados.
+	Entrada: Nome da cidade (str.upper()), Conjunto de dados (.csv) e Nome da variável (str);
+	Retorno: - Em tela (terminal), visualização do teste de tendência para determinada cidade,
+								tratando e não tratando sazonalidade.
+			 - Exibindo e salvando arquivo (.pdf) com a decompsição sazonal do conjunto de dados.
+	"""
 	decomposicao = sm.tsa.seasonal_decompose(csv[cidade], model = "additive", period = 52)
 	tendencia = decomposicao.trend
 	sazonalidade = decomposicao.seasonal
@@ -121,18 +219,23 @@ Valor $ p $: {round(dagostino_valor_p, 5)}\n""")
 	fig.suptitle(f"Decomposição Sazonal: {cidade} - {str_var.upper()}")
 	plt.xlabel("Semanas Epidemiológicas")
 	plt.tight_layout()
-	troca = {'Á': 'A', 'Â': 'A', 'À': 'A', 'Ã': 'A', 'Ä': 'A',
-             'É': 'E', 'Ê': 'E', 'È': 'E', 'Ẽ': 'E', 'Ë': 'E',
-             'Í': 'I', 'Î': 'I', 'Ì': 'I', 'Ĩ': 'I', 'Ï': 'I',
-             'Ó': 'O', 'Ô': 'O', 'Ò': 'O', 'Õ': 'O', 'Ö': 'O',
-             'Ú': 'U', 'Û': 'U', 'Ù': 'U', 'Ũ': 'U', 'Ü': 'U',
-             'Ç': 'C', " " : "_", "'" : "_", "-" : "_"}
-	for velho, novo in troca.items():
-		cidade = cidade.replace(velho, novo)
-	plt.savefig(f"{caminho_estatistica}distribuicao_{str_var.upper()}_{cidade}.pdf", format = "pdf", dpi = 1200)
-	print(f"\n\nSALVO COM SUCESSO!\n\n{caminho_estatistica}distribuicao_{str_var.upper()}_{cidade}.pdf\n\n")
-	#plt.show()
-	#plt.close(fig = None)
+	if _SALVAR == True:
+		troca = {'Á': 'A', 'Â': 'A', 'À': 'A', 'Ã': 'A', 'Ä': 'A',
+		         'É': 'E', 'Ê': 'E', 'È': 'E', 'Ẽ': 'E', 'Ë': 'E',
+		         'Í': 'I', 'Î': 'I', 'Ì': 'I', 'Ĩ': 'I', 'Ï': 'I',
+		         'Ó': 'O', 'Ô': 'O', 'Ò': 'O', 'Õ': 'O', 'Ö': 'O',
+		         'Ú': 'U', 'Û': 'U', 'Ù': 'U', 'Ũ': 'U', 'Ü': 'U',
+		         'Ç': 'C', " " : "_", "'" : "_", "-" : "_"}
+		_cidade = cidade.copy()
+		for velho, novo in troca.items():
+			_cidade = _cidade.replace(velho, novo)
+			plt.savefig(f"{caminho_estatistica}distribuicao_{str_var.upper()}_{_cidade}.pdf", format = "pdf", dpi = 1200)
+			print(f"\n\nSALVO COM SUCESSO!\n\n{caminho_estatistica}distribuicao_{str_var.upper()}_{cidade}.pdf\n\n")
+	if _VISUALIZAR == True:
+		print(f"\n\nVISUALIZANDO:\n\n{caminho_estatistica}distribuicao_{str_var.upper()}_{cidade}.pdf\n\n")
+		plt.show()
+		print(f"\n\nENCERRADO:\n\n{caminho_estatistica}distribuicao_{str_var.upper()}_{cidade}.pdf\n\n")
+
 	
 
 ### Visualização
@@ -142,7 +245,7 @@ for i in lista_cidades:
 
 for i in lista_cidades:
 	tendencia(i, focos)
-"""
+
 for i in lista_cidades:
 	teste_normal(i, focos, "focos")
 	teste_normal(i, casos, "casos")
@@ -150,6 +253,10 @@ for i in lista_cidades:
 	teste_normal(i, tmin, "tmin")
 	teste_normal(i, tmed, "tmed")
 	teste_normal(i, tmax, "tmax")
+"""
+
+histograma("FLORIANÓPOLIS", focos, "focos")
+
 
 ### Exibindo Informações
 print("\n \n FOCOS DE _Aedes_ spp. EM SANTA CATARINA - SÉRIE HISTÓRICA (DIVE/SC) \n")
