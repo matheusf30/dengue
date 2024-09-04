eaf3d31305c6c9f4a3dbb903f7c7bd32a85c9a4e
matheusf30 <mfsouza90@gmail.com>
2024-09-04 00:02:43 -0300

    Incrementando com as caixas de seleção. TESTAR EXECUÇÃO!

--- get_dengue.py
+++ get_dengue.py
@@ -5,6 +5,7 @@
 """
 
 # Bibliotecas correlatas
+from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import schedule
@@ -12,15 +13,33 @@ import time
 from datetime import datetime
 
 # Definindo Função
-url = "http://200.19.223.105/cgi-bin/tabnet?sinan/def/dengon.def"
+url = "http://200.19.223.105/cgi-bin/dh?sinan/def/dengon.def"
+url1 = "http://200.19.223.105/cgi-bin/tabnet?sinan/def/dengon.def"
+
 def download_dengue():
+	ano_atual = str(datetime.today().year)
 	hoje = datetime.today().strftime("%Y-%m-%d")
 	resposta = requests.get(url)
+	soup = BeautifulSoup(resposta.text, "html.parser")
+	form = soup.find("form")
+	form_action = form["action"]
+	"""
+	INVESTIGAÇÃO DENGUE A PARTIR DE 2014
+	Frequência por Mun infec SC e Sem.Epid.Sintomas
+	Classificacao Nova: Dengue com complicações, Febre Hemorrágica do Dengue, Síndrome do Choque do Dengue, Dengue, Dengue com sinais de alarme, Dengre grave
+	Conf.Desc pos2010: Laboratórial, Clínico-epidemiológico
+	Período:2014
+	"""
+	form_data = {"Linha": "Mun infec SC", "Coluna": "Sem.Epid.Sintomas",
+				"Períodos Disponíveis": ano_atual,
+				"Classificacao Nova" : ["Dengue com complicações", "Febre Hemorrágica do Dengue",
+										"Síndrome do Choque do Dengue", "Dengue",
+										"Dengue com sinais de alarme", "Dengre grave"],
+				"Conf.Desc pos2010" : ["Laboratórial", "Clínico-epidemiológico"]}
+	resposta = requests.post(f"http://200.19.223.105{form_action}", data = form_data)
 	if resposta.status_code == 200:
 		df = pd.read_html(resposta.text)
 		#df = df[0]
-	
-		# Save the DataFrame to a CSV file
 		arquivo = f"dengue_dive_{hoje}.csv"
 		df.to_csv(file_name, index=False)
 		print(f"Download realizado com sucesso e salvo como:\n{arquivo}")
