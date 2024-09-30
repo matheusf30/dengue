4b6b8b97fb7fe9213e859f5cddac249db586f661
matheusf30 <mfsouza90@gmail.com>
2024-09-30 19:17:44 -0300

    Implementando roteiro para gerar mapas temáticos de sazonalidade.

---
+++ cartografia_sazonal.py
@@ -0,0 +1,91 @@
+### Bibliotecas Correlatas
+import matplotlib.pyplot as plt 
+import matplotlib as mpl             
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point, Polygon
+import matplotlib.patches as mpatches
+from datetime import timedelta
+import numpy as np
+import seaborn as sns
+import statsmodels as sm
+import pymannkendall as mk
+import xarray as xr
+### Suporte
+import sys
+import os
+
+### Condições para Variar #######################################################
+
+_LOCAL = "IFSC" # OPÇÕES>>> "GH" "CASA" "IFSC"
+
+##################### Valores Booleanos ############ # sys.argv[0] is the script name itself and can be ignored!
+_AUTOMATIZAR = sys.argv[1]   # True|False                    #####
+_AUTOMATIZA = True if _AUTOMATIZAR == "True" else False      #####
+_VISUALIZAR = sys.argv[2]    # True|False                    #####
+_VISUALIZAR = True if _VISUALIZAR == "True" else False       #####
+_SALVAR = sys.argv[3]        # True|False                    #####
+_SALVAR = True if _SALVAR == "True" else False               #####
+##################################################################
+
+##### Padrão ANSI ###############################################################
+bold = "\033[1m"
+red = "\033[91m"
+green = "\033[92m"
+yellow = "\033[33m"
+blue = "\033[34m"
+magenta = "\033[35m"
+cyan = "\033[36m"
+white = "\033[37m"
+reset = "\033[0m"
+#################################################################################
+
+### Encaminhamento aos Diretórios
+if _LOCAL == "GH": # _ = Variável Privada
+	caminho_dados = "https://raw.githubusercontent.com/matheusf30/dados_dengue/main/"
+	caminho_modelos = "https://github.com/matheusf30/dados_dengue/tree/main/modelos"
+elif _LOCAL == "CASA":
+	caminho_dados = "C:\\Users\\Desktop\\Documents\\GitHub\\dados_dengue\\"
+	caminho_modelos = "C:\\Users\\Desktop\\Documents\\GitHub\\dados_dengue\\modelos\\"
+elif _LOCAL == "IFSC":
+	caminho_dados = "/home/sifapsc/scripts/matheus/dados_dengue/"
+	caminho_modelos = "/home/sifapsc/scripts/matheus/dados_dengue/modelos/"
+	caminho_resultados = "/home/sifapsc/scripts/matheus/dengue/resultados/modelagem/"
+	caminho_correlacao = "/home/sifapsc/scripts/matheus/dengue/resultados/correlacao/"
+	caminho_cartografia = "/home/sifapsc/scripts/matheus/dengue/resultados/cartografia/"
+else:
+	print("CAMINHO NÃO RECONHECIDO! VERIFICAR LOCAL!")
+print(f"\nOS DADOS UTILIZADOS ESTÃO ALOCADOS NOS SEGUINTES CAMINHOS:\n\n{caminho_dados}\n\n")
+
+### Renomeação das Variáveis pelos Arquivos
+casos = "sazonalidade_semanal_casos.csv"
+focos = "sazonalidade_semanal_focos.csv"
+prec = "sazonalidade_semanal_prec.csv"
+tmin = "sazonalidade_semanal_tmin.csv"
+tmed = "sazonalidade_semanal_tmed.csv"
+tmax = "sazonalidade_semanal_tmax.csv"
+
+### Abrindo Arquivo
+casos = pd.read_csv(f"{caminho_dados}{casos}", low_memory = False)
+focos = pd.read_csv(f"{caminho_dados}{focos}", low_memory = False)
+prec = pd.read_csv(f"{caminho_dados}{prec}", low_memory = False)
+tmin = pd.read_csv(f"{caminho_dados}{tmin}", low_memory = False)
+tmed = pd.read_csv(f"{caminho_dados}{tmed}", low_memory = False)
+tmax = pd.read_csv(f"{caminho_dados}{tmax}", low_memory = False)
+
+print(f"\n{green}SAZONALIDADE DE CASOS\n{reset}{casos}\n")
+print(f"\n{green}SAZONALIDADE DE FOCOS\n{reset}{focos}\n")
+print(f"\n{green}SAZONALIDADE DE PRECIPITAÇÃO\n{reset}{prec}\n")
+print(f"\n{green}SAZONALIDADE DE TEMPERATURA MÍNIMA\n{reset}{tmin}\n")
+print(f"\n{green}SAZONALIDADE DE TEMPERATURA MÉDIA\n{reset}{tmed}\n")
+print(f"\n{green}SAZONALIDADE DE TEMPERATURA MÁXIMA\n{reset}{tmax}\n")
+
+############### Base para Troca de Caracteres
+troca = {'Á': 'A', 'Â': 'A', 'À': 'A', 'Ã': 'A', 'Ä': 'A',
+		'É': 'E', 'Ê': 'E', 'È': 'E', 'Ẽ': 'E', 'Ë': 'E',
+		'Í': 'I', 'Î': 'I', 'Ì': 'I', 'Ĩ': 'I', 'Ï': 'I',
+		'Ó': 'O', 'Ô': 'O', 'Ò': 'O', 'Õ': 'O', 'Ö': 'O',
+		'Ú': 'U', 'Û': 'U', 'Ù': 'U', 'Ũ': 'U', 'Ü': 'U',
+		'Ç': 'C', " " : "_", "'" : "_", "-" : "_"}
+
+
