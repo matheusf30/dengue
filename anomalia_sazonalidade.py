7b1655a1cd0fc96687789231bd5a03a93ec8f769
matheusf30 <matheus.f30@aluno.ifsc.edu.br>
2024-10-09 14:21:35 -0300

    Implementando roteiro para plotar gráficos de sazonalidades e anomalias.

---
+++ anomalia_sazonalidade.py
@@ -0,0 +1,99 @@
+### Bibliotecas Correlatas
+import matplotlib.pyplot as plt 
+import matplotlib as mpl             
+import pandas as pd
+from datetime import timedelta
+import numpy as np
+import seaborn as sns
+import statsmodels as sm
+import pymannkendall as mk
+import xarray as xr
+### Suporte
+import sys
+import os
+
+### Condições para Variar #######################################################
+
+_LOCAL = "IFSC" # OPÇÕES>>> "GH" "CASA" "IFSC"
+
+##################### Valores Booleanos ############ # sys.argv[0] is the script name itself and can be ignored!
+_AUTOMATIZAR = sys.argv[1]   # True|False                    #####
+_AUTOMATIZA = True if _AUTOMATIZAR == "True" else False      #####
+_VISUALIZAR = sys.argv[2]    # True|False                    #####
+_VISUALIZAR = True if _VISUALIZAR == "True" else False       #####
+_SALVAR = sys.argv[3]        # True|False                    #####
+_SALVAR = True if _SALVAR == "True" else False               #####
+##################################################################
+
+_RETROAGIR = 16 # Semanas Epidemiológicas
+_ANO = "2023" # "2023" # "2022" # "2021" # "2020" # "total"
+_CIDADE = "Florianópolis" #"Florianópolis"#"Itajaí"#"Joinville"#"Chapecó"
+_METODO = "spearman" # "pearson" # "spearman" # "kendall"
+
+_CIDADE = _CIDADE.upper()
+
+##### Padrão ANSI ###############################################################
+bold = "\033[1m"
+red = "\033[91m"
+green = "\033[92m"
+yellow = "\033[33m"
+blue = "\033[34m"
+magenta = "\033[35m"
+cyan = "\033[36m"
+white = "\033[37m"
+reset = "\033[0m"
+#################################################################################
+
+### Encaminhamento aos Diretórios
+if _LOCAL == "GH": # _ = Variável Privada
+	caminho_dados = "https://raw.githubusercontent.com/matheusf30/dados_dengue/main/"
+	caminho_modelos = "https://github.com/matheusf30/dados_dengue/tree/main/modelos"
+elif _LOCAL == "CASA":
+	caminho_dados = "C:\\Users\\Desktop\\Documents\\GitHub\\dados_dengue\\"
+	caminho_modelos = "C:\\Users\\Desktop\\Documents\\GitHub\\dados_dengue\\modelos\\"
+elif _LOCAL == "IFSC":
+	caminho_dados = "/home/sifapsc/scripts/matheus/dados_dengue/"
+	caminho_modelos = "/home/sifapsc/scripts/matheus/dados_dengue/modelos/"
+	caminho_resultados = "/home/sifapsc/scripts/matheus/dengue/resultados/modelagem/"
+	caminho_correlacao = "/home/sifapsc/scripts/matheus/dengue/resultados/correlacao/"
+	caminho_cartografia = "/home/sifapsc/scripts/matheus/dengue/resultados/cartografia/"
+else:
+	print("CAMINHO NÃO RECONHECIDO! VERIFICAR LOCAL!")
+print(f"\nOS DADOS UTILIZADOS ESTÃO ALOCADOS NOS SEGUINTES CAMINHOS:\n\n{caminho_dados}\n\n")
+
+### Renomeação das Variáveis pelos Arquivos
+casos = "casos_dive_pivot_total.csv"  # TabNet/DiveSC
+focos = "focos_pivot.csv"
+prec = "prec_semana_ate_2023.csv"
+tmin = "tmin_semana_ate_2023.csv"
+tmed = "tmed_semana_ate_2023.csv"
+tmax = "tmax_semana_ate_2023.csv"
+
+### Abrindo Arquivo
+casos = pd.read_csv(f"{caminho_dados}{casos}", low_memory = False)
+focos = pd.read_csv(f"{caminho_dados}{focos}", low_memory = False)
+prec = pd.read_csv(f"{caminho_dados}{prec}", low_memory = False)
+tmin = pd.read_csv(f"{caminho_dados}{tmin}", low_memory = False)
+tmed = pd.read_csv(f"{caminho_dados}{tmed}", low_memory = False)
+tmax = pd.read_csv(f"{caminho_dados}{tmax}", low_memory = False)
+
+print(f"\n{green}CASOS\n{reset}{casos}\n")
+print(f"\n{green}FOCOS\n{reset}{focos}\n")
+print(f"\n{green}PRECIPITAÇÃO\n{reset}{prec}\n")
+print(f"\n{green}TEMPERATURA MÍNIMA\n{reset}{tmin}\n")
+print(f"\n{green}TEMPERATURA MÉDIA\n{reset}{tmed}\n")
+print(f"\n{green}TEMPERATURA MÁXIMA\n{reset}{tmax}\n")
+
+
+
+############### Base para Troca de Caracteres
+troca = {'Á': 'A', 'Â': 'A', 'À': 'A', 'Ã': 'A', 'Ä': 'A',
+		'É': 'E', 'Ê': 'E', 'È': 'E', 'Ẽ': 'E', 'Ë': 'E',
+		'Í': 'I', 'Î': 'I', 'Ì': 'I', 'Ĩ': 'I', 'Ï': 'I',
+		'Ó': 'O', 'Ô': 'O', 'Ò': 'O', 'Õ': 'O', 'Ö': 'O',
+		'Ú': 'U', 'Û': 'U', 'Ù': 'U', 'Ũ': 'U', 'Ü': 'U',
+		'Ç': 'C', " " : "_", "'" : "_", "-" : "_"}
+
+#################################################################################
+### Função Semana Epidemiológica (Semana que acabe no sábado tendo 4 dias iniciando no ano)
+#################################################################################
